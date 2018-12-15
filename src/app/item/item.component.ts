6bf0dabf6f2cee2baaa5ba094ce8ba2a107e4ff1
alex.paclin <alex.paclin@gmail.com>
2018-12-15 16:43:44 +0500

    ContentChild with #variable

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+
+export class ItemComponent implements OnInit, AfterContentInit {
 
   constructor() { }
+  @ContentChild('h') headerElement: ElementRef;
+
+  ngAfterContentInit(): void {
+    console.log(this.headerElement.nativeElement);
+  }
 
   ngOnInit() {
   }
