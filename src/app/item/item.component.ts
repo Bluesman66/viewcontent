f2ce1490a99443b56fc3ecffd385d510d695b022
alex.paclin <alex.paclin@gmail.com>
2018-12-15 18:40:58 +0500

    ViewChild

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,8 @@
 import {
-    AfterContentInit, Component, ContentChild, ContentChildren, ElementRef, OnInit, QueryList
+  AfterContentInit, Component, ContentChild, ContentChildren, ElementRef, OnInit, QueryList,
+  ViewChild,
+  ViewChildren,
+  AfterViewInit
 } from '@angular/core';
 
 import { NestedComponent } from '../nested/nested.component';
@@ -10,19 +13,34 @@ import { NestedComponent } from '../nested/nested.component';
   styleUrls: ['./item.component.css']
 })
 
-export class ItemComponent implements OnInit, AfterContentInit {
+export class ItemComponent implements OnInit, AfterContentInit, AfterViewInit {
 
   constructor() { }
-  @ContentChild('h') headerElement: ElementRef;
-  @ContentChild('n', { read: NestedComponent }) nestedComponentByRef: NestedComponent;
+
+  @ContentChild('hcontent') headerElement: ElementRef;
+  @ContentChild('ncontent', { read: NestedComponent }) nestedComponentByRef: NestedComponent;
   @ContentChild(NestedComponent) nestedComponent: NestedComponent;
   @ContentChildren(NestedComponent) nestedComponents: QueryList<NestedComponent>;
 
+  @ViewChild('hview') viewHeaderElement: ElementRef;
+  @ViewChild('nview', { read: NestedComponent }) viewNestedComponentByRef: NestedComponent;
+  @ViewChild(NestedComponent) viewNestedComponent: NestedComponent;
+  @ViewChildren(NestedComponent) viewNestedComponents: QueryList<NestedComponent>;
+
   ngAfterContentInit(): void {
+    console.log('--- AfterContentInit ---');
     console.log(this.headerElement.nativeElement);
     console.log(this.nestedComponentByRef);
     console.log(this.nestedComponent);
     console.log(this.nestedComponents.toArray());
+  }
+
+  ngAfterViewInit(): void {
+    console.log('--- AfterViewInit ---');
+    console.log(this.viewHeaderElement.nativeElement);
+    console.log(this.viewNestedComponentByRef);
+    console.log(this.viewNestedComponent);
+    console.log(this.viewNestedComponents.toArray());
   }
 
   ngOnInit() {
